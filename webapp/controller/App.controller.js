3d85e6d85c404f04035584aa22337f9c0f5f4607
Srinu Kallepalli <srinukallepalli16@gmail.com>
2025-01-09 20:58:25 +0000

    Initial Changes

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.app.sampleapp.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/app/sampleapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
